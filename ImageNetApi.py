fed064977f65ff5e8575b8f9d0b2ce0f413b7516
xvshubjb <xvshubjb>
2018-08-14 11:12:34 +0800

    add login

--- ImageNetApi.py
+++ ImageNetApi.py
@@ -1,18 +1,23 @@
-from flask import Flask, jsonify, abort, make_response, request, url_for,render_template,send_file
+from flask import Flask, jsonify, abort, make_response, request, url_for,render_template,send_file,session
+from flask import redirect
 from flask_httpauth import HTTPBasicAuth
 from Parameter import http_state,img_file_path
 from Resnet_50.train import AiResNet50
 from Resnet_50.test import AiResNet50Predict
 from Parameter import httpResultWhiteMsg
+from Parameter import Parameters
+from datetime import timedelta
 import io
 import uuid
 import os
 import cv2
 
-
+pass_urls=["static","login","logout"]
 app = Flask(__name__)
 # 图片最大为512M
 app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
+app.config['SECRET_KEY']=os.urandom(24)   #设置为24位的字符,每次运行服务器都是不同的，所以服务器启动一次上次的session就清除。
+app.config['PERMANENT_SESSION_LIFETIME']=timedelta(days=7) #设置session的保存时间。
 auth = HTTPBasicAuth()
 
 #设置post请求中获取的图片保存的路径
@@ -27,6 +32,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/index')
+def index_2():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session["user"]=="";
+    return render_template("login.html")
+
+
+@app.route('/login/user', methods=['POST'])
+def login_user():
+    user_name = request.form["user_name"]
+    pass_word = request.form["pass_word"]
+    print("login user:%s pass:%s"%(user_name,pass_word))
+    if(user_name==Parameters.user  and pass_word==Parameters.pwd):
+        session["user"]=user_name;
+        print("login user:%s pass:%s is pass"%(user_name,pass_word))
+        return redirect(url_for("index"))
+    else:
+        return httpResultWhiteMsg.send("用户名密码错误")
+
 
 #返回目前平台能识别的所有类别
 @app.route('/ai/class/getall', methods=['GET'])
@@ -125,8 +157,27 @@ def not_found(error):
     return make_response(jsonify({'error': 'Invalid data!'}), 400)
 
 def _serve_pil_image(pil_img):
-
     return send_file(pil_img, mimetype='image/jpg', cache_timeout=0)
 
+@app.before_request
+def before_user():
+    url = request.url
+    print(url)
+    in_pass=False
+    for one_pass in pass_urls:
+        if(one_pass in url):
+            pass
+            print("url in pass_urls")
+            in_pass=True
+
+    if(not in_pass):
+        print("url not in pass_urls")
+        if(session.get("user")==Parameters.user):
+            print("session check pass")
+            pass
+        else:
+            print("session check fail")
+            return render_template("login.html")
+
 if __name__ == '__main__':
-    app.run(host='172.30.53.250', port=8099)
+    app.run(host=Parameters.host, port=Parameters.port)
--- Parameter.py
+++ Parameter.py
@@ -9,4 +9,12 @@ class img_file_path:
 class httpResultWhiteMsg:
     @staticmethod
     def send(value):
-        return '<div style="color:#fff;font-size:36px;">'+value+'</div>'
+        return '<div style="color:red;font-size:36px;">'+value+'</div>'
+
+class Parameters:
+    host='172.30.53.250'
+    port=8899
+    model_path="D:\\Data\\ai\\model\\traffic_sign.model"
+    user="admin"
+    pwd="admin"
+
